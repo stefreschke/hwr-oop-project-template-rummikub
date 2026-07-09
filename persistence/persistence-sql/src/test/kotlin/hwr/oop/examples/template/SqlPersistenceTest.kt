64b8f08c34bafa4b67391c3202b89cfe22ec2b3f
Eliss433 <s_zawal25@stud.hwr-berlin.de>
2026-07-09 22:06:03 +0200

    test: align sql persistence error message

--- persistence/persistence-sql/src/test/kotlin/hwr/oop/examples/template/SqlPersistenceTest.kt
+++ persistence/persistence-sql/src/test/kotlin/hwr/oop/examples/template/SqlPersistenceTest.kt
@@ -65,7 +65,7 @@ class SqlPersistenceTest {
 		// when
 		// then
 		assertThatThrownBy { adapter.load(gameId) }
-			.hasMessageContaining("Game $gameId not found")
+			.hasMessageContaining("Game not found: $gameId")
 	}
 }
 
